2b300244e5aedc862d6635fd901e06514163e803
Andrew Chernuha <actimel333@gmail.com>
2025-03-19 22:28:06 +0200

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,14 @@ import { useRef, useState } from "react";
 export default function Home() {
   const [image, setImage] = useState(null);
   const [response, setResponse] = useState("");
-  const videoRef = useRef(null);
-  const canvasRef = useRef(null);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
   const startCamera = async () => {
     try {
       const stream = await navigator.mediaDevices.getUserMedia({ video: true });
       if (videoRef.current) {
-        videoRef.current.srcObject = stream;
+        videoRef.current!.srcObject = stream;
       }
     } catch (error) {
       console.error("Error accessing camera:", error);
